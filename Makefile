2b369b25f550328a52be1520fe287db3db819d01
Irina <irinagomaturana@gmail.com>
2021-12-01 09:10:09 +0100

    esquelet laberint

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 #OPCIONS = -D_JUDGE_ -D_GLIBCXX_DEBUG -Wall -Wextra -Wno-sign-compare -std=c++11
 OPCIONS = -D_JUDGE_ -D_GLIBCXX_DEBUG -O2 -Wall -Wextra -Wno-sign-compare -std=c++11
-all: main.exe driver_maze.exe
+all: main.exe  #driver_maze.exe
 
-main.exe: main.o cambra.o paret.hpp
-	g++ -o main.exe main.o cambra.o paret.hpp -lesin
+main.exe: main.o cambra.o paret.hpp laberint.o
+	g++ -o main.exe main.o cambra.o paret.hpp laberint.o -lesin
 
-driver_maze.exe: driver_maze.o cambra.o paret.hpp
-	g++ -o main.exe driver_maze.o cambra.o paret.hpp -lesin
+#driver_maze.exe: driver_maze.o cambra.o paret.hpp
+#g++ -o main.exe driver_maze.o cambra.o paret.hpp -lesin
 
 #	Descomenteu les següents línies si voleu executar tots
 #	els jocs de proves.
@@ -20,14 +20,16 @@ driver_maze.exe: driver_maze.o cambra.o paret.hpp
 main.o: main.cpp cambra.hpp
 	g++ -c main.cpp $(OPCIONS)
 	
-driver_maze.o: driver_maze.cpp 
-	g++ -c driver_maze.cpp $(OPCIONS)
+#driver_maze.o: driver_maze.cpp 
+# g++ -c driver_maze.cpp $(OPCIONS)
 #paret.o: paret.hpp
 #	g++ -c paret.hpp $(OPCIONS)
 
 cambra.o: cambra.cpp cambra.hpp
 	g++ -c cambra.cpp $(OPCIONS)
 
+laberint.o: laberint.cpp laberint.hpp
+	g++ -c laberint.cpp $(OPCIONS)
 
 #Magatzem.o: Magatzem.cpp Magatzem.hpp
 #	g++ -c Magatzem.cpp $(OPCIONS)
--- laberint.cpp
+++ laberint.cpp
@@ -1,1 +1,68 @@
 #include "laberint.hpp"
+#include <iostream>
+
+using namespace std;
+ // Constructora d'un laberint buit sense excavar (sense cap porta oberta)
+  // de la mida indicada. Totes les cambres del laberint no tenen cap porta.
+  // Es produeix un error si el número de files o de columnes del laberint és 0.
+   laberint::laberint(nat num_fil=5, nat num_col=5) throw(error){
+
+
+   }
+
+  // Constructora d'un laberint a partir d'un istream (canal d'entrada) is.
+  // El format de l'istream seguirà l'exposat a l'apartat 2.3. Es presuposa que
+  // el laberint és correcte.
+   laberint::laberint(std::istream & is) throw(error){
+
+   }
+
+  // Constructora per còpia, assignació i destructora.
+  laberint::laberint(const laberint & l) throw(error){
+
+  }
+  laberint::laberint & operator=(const laberint & l) throw(error){
+
+  }
+  laberint::~laberint() throw(){
+
+  }
+
+  // Retornen el número de files i columnes que té el laberint, respectivament.
+  nat laberint::num_files() const throw(){
+
+  }
+  nat laberint::num_columnes() const throw(){
+
+  }
+
+  // Retorna la cambra situada a la posició especificada per pos.
+  // Aquest mètode permet utilitzar el laberint d'aquesta manera:
+  //   laberint l(3,4);
+  //   posicio pos(2,2);
+  //   cambra c = l(pos);
+  // Es produeix un error si la posició donada no existeix al laberint.
+  cambra laberint::operator()(const posicio & pos) const throw(error){
+
+  }
+
+  // Obre la porta en la paret donada de la cambra que està especificada per pos.
+  // També obre la porta corresponent en la cambra adjacent. Es produeix un error
+  // si la posició no existeix o no es pot obrir una porta en la direcció
+  // indicada perquè dóna a l'exterior.
+  void laberint::obre_porta(paret p, const posicio & pos) throw(error){
+
+  }
+
+  // Tanca la porta en la paret donada de la cambra que està especificada per pos.
+  // També tanca la porta corresponent en la cambra adjacent. Es produeix un error
+  // si la posició no existeix.
+  void laberint::tanca_porta(paret p, const posicio & pos) throw(error){
+
+  }
+
+  // Escriu el laberint a l'ostream (canal de sortida) os. El format per escriure
+  // el laberint seguirà l'exposat a l'apartat 2.3.
+  void laberint::print(std::ostream & os) const throw(){
+
+  }
